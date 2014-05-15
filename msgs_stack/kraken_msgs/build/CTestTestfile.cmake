0d3daef20085a0f8ed026a8a040bd15d03d765a9
Prudhvi Dharmana <dharmana.prudhvi@gmail.com>
2014-05-15 22:01:09 +0530

    kraken_msgs addition

--- msgs_stack/kraken_msgs/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prudhvi/ros_ws/kraken_msgs
-# Build directory: /home/prudhvi/ros_ws/kraken_msgs/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
