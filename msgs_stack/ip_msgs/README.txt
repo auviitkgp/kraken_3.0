3bcd35b9f509035451f30ce5085a7d68d6318900
Prudhvi Dharmana <dharmana.prudhvi@gmail.com>
2014-05-24 01:18:07 +0530

    clean repo with .gitignore

---
+++ msgs_stack/ip_msgs/README.txt
@@ -0,0 +1,2 @@
+Add any action file for IP task here.
+Additional package required is actionlib.
---
+++ msgs_stack/ip_msgs/src/ip_msgs/msg/__init__.py
@@ -0,0 +1,7 @@
+from ._markerAction import *
+from ._markerResult import *
+from ._markerActionFeedback import *
+from ._markerFeedback import *
+from ._markerActionGoal import *
+from ._markerActionResult import *
+from ._markerGoal import *
