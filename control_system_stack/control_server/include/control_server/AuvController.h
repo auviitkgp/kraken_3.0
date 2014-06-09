44f9f8e1dad5864dc4a6ad9e8b894c6a3cb1b8ea
vds <home@ubuntu>
2014-06-09 16:48:05 +0530

    AuvController modified,pid method changed

--- control_system_stack/control_server/include/control_server/AuvController.h
+++ control_system_stack/control_server/include/control_server/AuvController.h
@@ -1,5 +1,6 @@
 #ifndef AUVCONTROLLER_H
 #define AUVCONTROLLER_H
+
 #include <controller_basic/StateController.h>
 #include <pose_server/KrakenPose.h>
 #include <kraken_msgs/thrusterData6Thruster.h>
@@ -20,7 +21,9 @@ namespace kraken_controller
       void moveForward();
       void moveBack();
       void pause();
+      void moveTest();
       void loadParams(const std::vector<std::string> &filenames);
+      bool checkError(const kraken_msgs::krakenPose &msg);
     private:
       std::vector<ControlParameters*> _control_parameters;
       std::map<std::string,int> _control_parameters_index;
