246b40d41c918c913063b23b27e6bcc25e8af269
Nevin Valsaraj <nevthinking@gmail.com>
2014-06-19 00:31:49 +0530

    - Add joystick control to simulator via new  module joystick_control - Add simulator(joystick control mode) launcher script

---
+++ Scripts/runKraken_joystick.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+WS=$ROS_WORKSPACE 
+source /opt/ros/hydro/setup.bash
+export ROS_WORKSPACE=$WS
+export ROS_PACKAGE_PATH=$ROS_WORKSPACE:$ROS_PACKAGE_PATH
+
+
+#Run kraken_visualisation module
+roscd
+cd kraken_3.0/Scripts/
+./kraken_launcher &
+
+#Run simulator physics model
+rosrun simulator_physics_model start_auv_model &
+
+#Pass parameter for the joystick setup
+#Use path to joystick here
+rosparam set joy_node/dev "/dev/input/js1"
+
+#Start joystick publisher module
+rosrun joy joy_node &
+
+#Run joystick controller module
+rosrun joystick_control joystick_control
---
+++ simulator_stack/joystick_control/src/joystick_control.cpp
@@ -0,0 +1,47 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <kraken_msgs/forceData6Thruster.h>
+#include <sensor_msgs/Joy.h>
+
+using namespace std;
+kraken_msgs::forceData6Thruster _force_sent;
+float _received[11];
+
+void joyCB(const sensor_msgs::JoyConstPtr &msg)
+{
+
+    _received[0] = msg->axes[0];
+    _received[1] = msg->axes[1];
+    _received[2] = msg->axes[2];
+    _received[3] = msg->axes[3];
+    _received[4] = msg->axes[4];
+
+    _received[5] = msg->buttons[0];
+    _received[6] = msg->buttons[1];
+    _received[7] = msg->buttons[2];
+    _received[8] = msg->buttons[3];
+    _received[9] = msg->buttons[4];
+    _received[10] = msg->buttons[5];
+}
+
+int main(int argc, char *argv[]){
+    ros::init(argc, argv, "joystick_control");
+	ros::NodeHandle _nh;
+    ros::Publisher _force_pub = _nh.advertise<kraken_msgs::forceData6Thruster>("/kraken/forceData6Thruster", 100);
+    ros::Subscriber _joy_sub = _nh.subscribe<sensor_msgs::Joy>("/joy", 100, joyCB);
+    ros::Rate _looprate(10);
+
+    while(ros::ok())
+    {
+        _force_sent.data[0] = _received[1] * 16.0;
+        _force_sent.data[1] = _received[1] * 16.0;
+        _force_sent.data[2] = - _received[3] * 16.0 - _received[0];
+        _force_sent.data[3] = - _received[3] * 16.0 + _received[0];
+        _force_sent.data[4] = _received[4] * 12;
+        _force_sent.data[5] = _received[4] * 12;
+        _force_pub.publish(_force_sent);
+         ros::spinOnce();
+        _looprate.sleep();
+    }
+	return 0;
+}
