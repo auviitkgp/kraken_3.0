0bbd2a6c35f5c5a12e278099514c31074caf3595
Nevin Valsaraj <nevthinking@gmail.com>
2014-06-23 07:52:11 +0530

    Modify videoread module to accept video filepath and target topic name as parameters

--- vision_stack/camera/videoread/src/videoread.cpp
+++ vision_stack/camera/videoread/src/videoread.cpp
@@ -12,20 +12,22 @@ using namespace cv;
 int main(int argc, char ** argv)
 {
 	ros::init(argc, argv, "videoread");
+
+    if(argc != 3)
+    {
+        cout << "videoread : Requires 1) video file path, 2)target topic name as arguments." << endl;
+        ros::shutdown();
+    }
+    
+    std::string _videopath = argv[1];
+    std::string _topicname = argv[2];
+
     ros::NodeHandle _n;
     image_transport::ImageTransport _it(_n);
-    image_transport::Publisher _pub = _it.advertise("/kraken/debug/videoread_image", 1);
+    image_transport::Publisher _pub = _it.advertise(_topicname, 1);
     sensor_msgs::ImagePtr _publishImage;
     cv_bridge::CvImage _image;
     ros::Rate _looprate(10);
-
-    if(argc != 2)
-    {
-        cout << "videoread : Requires video file path as argument." << endl;
-        ros::shutdown();
-    }
-
-    std::string _videopath = argv[1];
 
     VideoCapture _camera(_videopath.c_str());
     if(_camera.isOpened())
