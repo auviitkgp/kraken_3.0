900ed1d17a8b4c7104fcf211f38bb821813bc94c
Nevin Valsaraj <nevthinking@gmail.com>
2014-06-25 04:18:42 +0530

    Adding new package task_buoy for the buoy task to integrate buoy related packages

---
+++ vision_stack/task_buoy/include/task_buoy/taskheader.h
@@ -0,0 +1,11 @@
+#ifndef TASK_H
+#define TASK_H
+
+#define DETECT_MARKER 0
+#define ALIGN_MARKER 1
+#define DETECT_BUOY 2
+#define ALIGN_BUOY 3
+#define DETECT_VGATE 4
+#define ALIGN_VGATE 5
+
+#endif
