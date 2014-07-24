fb5da21f92a851a3f775ed2c94b212fbba10b5b5
prudhvid <dharmana.prudhvi@gmail.com>
2014-07-24 08:16:44 +0530

    added resources to all manifest files added file makeKraken.sh to build all packages in a single blow

---
+++ Scripts/makekraken.sh
@@ -0,0 +1,7 @@
+WS=$ROS_WORKSPACE 
+source /opt/ros/hydro/setup.bash
+export ROS_WORKSPACE=$WS
+export ROS_PACKAGE_PATH=$ROS_WORKSPACE:$ROS_PACKAGE_PATH
+
+
+rosmake sensor_stack mission_planner_stack control_system_stack msgs_stack gui_stack hardware_stack --threads 4
