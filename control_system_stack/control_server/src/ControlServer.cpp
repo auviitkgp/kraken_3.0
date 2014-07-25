f76c0bf84133c85731f84293eb7796f393a7cc95
icyflame <kannan.siddharth12@gmail.com>
2014-07-25 22:39:22 +0530

    Changed controlserver file

--- control_system_stack/control_server/src/ControlServer.cpp
+++ control_system_stack/control_server/src/ControlServer.cpp
@@ -10,7 +10,7 @@ namespace kraken_controller
     _pub6 = n.advertise<kraken_msgs::thrusterData6Thruster>(topics::CONTROL_PID_THRUSTER6,2);
     _set_point = n.advertise<kraken_msgs::krakenPose>("/kraken/pos_set",2);
     _sub_pose = n.subscribe<kraken_msgs::krakenPose>(topics::CONTROL_POSE_ESTIMATED,2,&ControlServer::poseFeedBack,this);
-    _sub_ip_error  = n.subscribe<kraken_msgs::ipControllererror>("/kraken/ip_error_data",2,&ControlServer::ipErrorFeedBack,this);
+    _sub_ip_error  = n.subscribe<kraken_msgs::ipControllererror>(topics::CONTROL_IP_ERROR,2,&ControlServer::ipErrorFeedBack,this);
 
     _do_control = true;
     _ip_controller = false;
