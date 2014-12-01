3e8731b4716b80caac3667e4e9e510571eb0baa8
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-12-01 16:30:10 +0530

    Change the name of the output file to scene.urdf.
    Add instructions on where to copy the file that is generated by the GUI.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- simulator_stack/simulator-drag-drop-gui/rundragdrop.sh
+++ simulator_stack/simulator-drag-drop-gui/rundragdrop.sh
@@ -3,3 +3,6 @@ python main.py
 echo "Running the fileWrite.py script"
 python fileWrite.py
 mv output.xml input.xml
+cp input.xml scene.urdf
+echo "Complete."
+echo "Overwrite scene.urdf file in kraken_visualization/kraken/scenes/scene.urdf with the file here."
