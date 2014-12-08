68e5df26c69d2d1f381d31689a5ac8b5410d4ca8
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-12-08 19:50:59 +0530

    Change the names of the buttons that appear in the GUI.

--- simulator_stack/joystick_control/src/main.py
+++ simulator_stack/joystick_control/src/main.py
@@ -60,13 +60,13 @@ frame = Frame(root, width=100, height=100)
 left = Button(frame, text="left", command=create_callbacks(0))
 left.grid(row=1,column=0)
 
-down = Button(frame, text="down", command=create_callbacks(2))
+down = Button(frame, text="backward", command=create_callbacks(2))
 down.grid(row=1,column=1)
 
 right = Button(frame, text="right", command=create_callbacks(1))
 right.grid(row=1,column=2)
 
-top = Button(frame, text="top", command=create_callbacks(3))
+top = Button(frame, text="forward", command=create_callbacks(3))
 top.grid(row=0,column=1)
 
 root.bind("a", create_callbacks(0))
