f490ebe4c45df60c04e84559d3de217ec2f0bda2
prudhvid <dharmana.prudhvi@gmail.com>
2015-03-31 23:37:38 +0530

    merge branch niot-march and indigo-devel
    hardcoded buoy task in depthcontrol.py and control.py

--- sensor_stack/seabotix/src/control.py
+++ sensor_stack/seabotix/src/control.py
@@ -9,10 +9,13 @@ from kraken_msgs.msg import thrusterData6Thruster
 from kraken_msgs.msg import thrusterData4Thruster
 from kraken_msgs.msg import imuData
 from resources import topicHeader
+from actionmsg.msg import buoyActionFeedback
+
+PIXEL_RATIO=100.
 
 yaw = 0.0
-goal = 150.0
-
+goal = 0.0
+offset=0
 Kp_left = 1.27;
 Kd_left = 0.016;
 Ki_left = 0.00;
@@ -37,34 +40,61 @@ def imuCB(dataIn):
 
 	prevError = errorP
 	errorP = goal - yaw
-	print errorP
+	fabserror=abs(errorP)
+	sign=1;
+	if(errorP<0):
+		sign=-1;
+	if(fabserror>180):
+		errorP=-1*(sign*(360-fabserror));
+	
+
+
+	print "Error P:", errorP, "; Yaw: ", yaw
 	errorI = errorP + prevError
 	errorD = errorP - prevError
 
 #thruster6Data.data = [0.0,0.0,0.0,0.0,0.0,0.0]
 #thruster4Data.data = [0.0, 0.0, 0.0, 0.0]
 
+def goalCB(goalin):
+	global goal
+	goalin=goalin.feedback
+	goal=(goal+ goalin.errorx/PIXEL_RATIO)%360
+	print 'goal_new= ',goal,'goalin= ',goalin
+	
+
+
 
 if __name__ == '__main__':
+	# offset=50;
 	thruster4Data=thrusterData4Thruster();
 	thruster6Data=thrusterData6Thruster();
 	
 	rospy.init_node('Control', anonymous=True)
 	sub = rospy.Subscriber(topicHeader.SENSOR_IMU, imuData, imuCB)
+
+	goalsub=rospy.Subscriber('/buoy/feedback',buoyActionFeedback,goalCB)
+
 	pub4 = rospy.Publisher(topicHeader.CONTROL_PID_THRUSTER4, thrusterData4Thruster, queue_size = 2)
 	pub6 = rospy.Publisher(topicHeader.CONTROL_PID_THRUSTER6, thrusterData6Thruster, queue_size = 2)
 
 	r = rospy.Rate(10)
-
+	thruster6Data.data[0] = 0.0
+	thruster6Data.data[1] = 0.0
+	thruster6Data.data[2] = 0.0
+	thruster6Data.data[3] = 0.0
 	while not rospy.is_shutdown():
 
-		thruster6Data.data[0] = 0.0
-		thruster6Data.data[1] = 0.0
-		thruster6Data.data[2] = 0.0
-		thruster6Data.data[3] = 0.0
+		
 
-		thruster6Data.data[4] = Kp_left*errorP + Kd_left*errorD + Ki_left*errorI
-		thruster6Data.data[5] = Kp_right*errorP + Kd_right*errorD + Ki_right*errorI
+		thruster6Data.data[4] =Kp_left*errorP + Kd_left*errorD + Ki_left*errorI
+		thruster6Data.data[5] =Kp_right*errorP + Kd_right*errorD + Ki_right*errorI
+
+		if(abs(errorP)<10):
+			thruster6Data.data[4]=offset;
+			thruster6Data.data[5]=offset;			
+			# print 'mind fuck'
+
 
 		thruster4Data.data[0] = thruster6Data.data[0]
 		thruster4Data.data[1] = thruster6Data.data[1]
