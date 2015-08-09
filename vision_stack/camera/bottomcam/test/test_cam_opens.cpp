24ab6143abd30a2a2f56a90c68b0bddad35ff0c6
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-09 22:31:07 +0530

    Add meaningful tests
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- vision_stack/camera/bottomcam/test/test_cam_opens.cpp
+++ vision_stack/camera/bottomcam/test/test_cam_opens.cpp
@@ -1,21 +1,30 @@
 // Bring in my package's API, which is what I'm testing
-// #include "../src/bottomcam.cpp"
+#include "bottomcam/bottomcam.h"
 // Bring in gtest
 #include <gtest/gtest.h>
 
 // Declare a test
-TEST(TestSuite, testCase1)
+TEST(CameraTest, EnsureCameraConstructorWorks)
 {
-  EXPECT_EQ(1, 1); // 1 == 1
-  EXPECT_GT(10, 0); // 10 > 0
+  Camera _camObj;
+  EXPECT_EQ(_camObj.getCurrentCameraNo(), 0);
+  EXPECT_EQ(_camObj.isOpen(), false);
+  Camera _camObj_1(1);
+  EXPECT_EQ(_camObj_1.getCurrentCameraNo(), 1);
 }
-/*
-// Declare another test
-TEST(TestSuite, testCase2)
+
+TEST(CameraTest, EnsureCameraIsSendingImages)
 {
-<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+  Camera _camObj;
+  cv::VideoCapture _camera = _camObj.returnCamera();
+  cv::Mat _frame;
+  _camera.open(0);
+  _camera >> _frame;
+  EXPECT_EQ(_frame.empty(), false);
+  EXPECT_GT(_frame.size().height, 0);
+  EXPECT_GT(_frame.size().width, 0);
 }
-*/
+
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
