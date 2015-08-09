3490a1ee2842d98924d509765f021cbceba2e606
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-09 22:31:22 +0530

    Remove unnecessary code.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- vision_stack/camera/bottomcam/include/bottomcam/bottomcam.h
+++ vision_stack/camera/bottomcam/include/bottomcam/bottomcam.h
@@ -31,4 +31,3 @@ public:
 
   void inspect();
 };
-
--- vision_stack/camera/bottomcam/src/main.cpp
+++ vision_stack/camera/bottomcam/src/main.cpp
@@ -1,12 +1,3 @@
-// #include <ros/ros.h>
-// #include <image_transport/image_transport.h>
-// #include <cv_bridge/cv_bridge.h>
-// #include <sensor_msgs/image_encodings.h>
-// #include <opencv2/imgproc/imgproc.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <std_msgs/String.h>
-//
-// #include <resources/topicHeader.h>
 #include "bottomcam/bottomcam.h"
 
 Camera camObj;
