4ecb7e0ac160b8d0ee8691b347e9ce4783b7e27b
Kalyan Kumar <kalyankumar.garapaty@gmail.com>
2015-10-30 23:27:24 +0530

    [AUTO-FORMAT]

--- vision_stack/task_buoy/src/task_buoy_new.cpp
+++ vision_stack/task_buoy/src/task_buoy_new.cpp
@@ -16,23 +16,23 @@ double current_yaw;
 
 void getyawCallback(const kraken_msgs::absoluteRPY& msg)
 {
-	current_yaw = msg.yaw;
+    current_yaw = msg.yaw;
 }
 
 int main(int argc, char ** argv)
 {
-	ros::init(argc, argv, "buoy_node");
+    ros::init(argc, argv, "buoy_node");
 
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe(topics::ABSOLUTE_RPY, 1000, getyawCallback);
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe(topics::ABSOLUTE_RPY, 1000, getyawCallback);
 
-	// Start Service
-	
-	Client detect_client("buoy", true);
-	ROS_INFO("buoy_client has started ... Waiting for the server to start");
-	detect_client.waitForServer();
-	ROS_INFO("buoy_server has started.");
-	actionmsg::buoyGoal _goal;
+    // Start Service
+
+    Client detect_client("buoy", true);
+    ROS_INFO("buoy_client has started ... Waiting for the server to start");
+    detect_client.waitForServer();
+    ROS_INFO("buoy_server has started.");
+    actionmsg::buoyGoal _goal;
     _goal.order = DETECT_BUOY;
     ROS_INFO("Sending goal - DETECT_BUOY.");
     detect_client.sendGoal(_goal);
@@ -40,7 +40,7 @@ int main(int argc, char ** argv)
 
     if(_actionStatus == true)
     {
-    	// Stop Service
+        // Stop Service
 
         actionlib::SimpleClientGoalState _state = detect_client.getState();
         ROS_INFO("buoy_client : Action finished - %s",_state.toString().c_str());
@@ -50,14 +50,14 @@ int main(int argc, char ** argv)
 
         Client1 yaw_client("setYaw", true);
         ROS_INFO("yaw_client has started ... Waiting for the server to start");
-		yaw_client.waitForServer();
-		ROS_INFO("yaw_server has started.");
-		kraken_msgs::setYawGoal _goal1;
-		_goal1.yaw = current_yaw;
-		ROS_INFO("Secding goal to set yaw at %f", current_yaw);
-		yaw_client.sendGoal(_goal1);
-		_actionStatus = yaw_client.waitForResult();
-	
+        yaw_client.waitForServer();
+        ROS_INFO("yaw_server has started.");
+        kraken_msgs::setYawGoal _goal1;
+        _goal1.yaw = current_yaw;
+        ROS_INFO("Secding goal to set yaw at %f", current_yaw);
+        yaw_client.sendGoal(_goal1);
+        _actionStatus = yaw_client.waitForResult();
+
     }
     else
     {
