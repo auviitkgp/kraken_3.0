b8509a0176bee8fb628541709d37bc443b35f791
Kalyan Kumar <kalyankumar.garapaty@gmail.com>
2015-12-09 18:09:08 +0530

    Finish logging.
    Signed-off-by: Kalyan Kumar <kalyankumar.garapaty@gmail.com>

--- vision_stack/task_buoy/src/task_buoy_new.cpp
+++ vision_stack/task_buoy/src/task_buoy_new.cpp
@@ -33,7 +33,7 @@ Client2 scan_start("scanning_server", true);
 
 void my_handler(int s)
 {
-    printf("Caught Signal %d\n", s);
+    ROS_DEBUG("Caught Signal %d\n", s);
     detect_client.cancelGoal();
     yaw_client.cancelGoal();
     scan_start.cancelGoal();
@@ -59,21 +59,21 @@ int main(int argc, char ** argv)
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe(topics::ABSOLUTE_RPY, 1000, getyawCallback);
 
-    ROS_INFO("Scanning client has started ... Waiting for the server to start");
+    ROS_DEBUG("Scanning client has started ... Waiting for the server to start");
     scan_start.waitForServer();
-    ROS_INFO("Scanning server has started");
+    ROS_DEBUG("Scanning server has started");
     kraken_msgs::scanGoal _goal2;
     _goal2.direction = -1;
     _goal2.speed = -1;
-    ROS_INFO("Sending angular speed for the bot to rotate at.");
+    ROS_DEBUG("Sending angular speed for the bot to rotate at.");
     scan_start.sendGoal(_goal2);
 
-    ROS_INFO("buoy_client has started ... Waiting for the server to start");
+    ROS_DEBUG("buoy_client has started ... Waiting for the server to start");
     detect_client.waitForServer();
-    ROS_INFO("buoy_server has started.");
+    ROS_DEBUG("buoy_server has started.");
     actionmsg::buoyGoal _goal;
     _goal.order = DETECT_BUOY;
-    ROS_INFO("Sending goal - DETECT_BUOY.");
+    ROS_DEBUG("Sending goal - DETECT_BUOY.");
     detect_client.sendGoal(_goal);
 
     // The duration given here should be the time taken by AUV to complete one full rotation.
@@ -86,17 +86,17 @@ int main(int argc, char ** argv)
     	scan_start.cancelGoal();
 
         actionlib::SimpleClientGoalState _state = detect_client.getState();
-        ROS_INFO("buoy_client : Action finished - %s",_state.toString().c_str());
+        ROS_DEBUG("buoy_client : Action finished - %s",_state.toString().c_str());
 
         // Get the yaw here and store it
         // Global variable is being set to yaw whenever callback function is called
 
-        ROS_INFO("yaw_client has started ... Waiting for the server to start");
+        ROS_DEBUG("yaw_client has started ... Waiting for the server to start");
         yaw_client.waitForServer();
-        ROS_INFO("yaw_server has started.");
+        ROS_DEBUG("yaw_server has started.");
         kraken_msgs::setYawGoal _goal1;
         _goal1.yaw = current_yaw;
-        ROS_INFO("Secding goal to set yaw at %f", current_yaw);
+        ROS_DEBUG("Secding goal to set yaw at %f", current_yaw);
         yaw_client.sendGoal(_goal1);
         //_actionStatus = yaw_client.waitForResult();
         return 0;
@@ -104,7 +104,7 @@ int main(int argc, char ** argv)
     }
     else
     {
-        ROS_INFO("buoy_client : No buoy found in the area");
+        ROS_DEBUG("buoy_client : No buoy found in the area");
         detect_client.cancelGoal();
         scan_start.cancelGoal();
     }
