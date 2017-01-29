2c0973763b076127d99df6c193d1a586cbef2f67
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-01-29 12:50:01 +0530

    resources: Remove all the generated files from this package

--- resources/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/dyutimoy/catkin_ws/src/kraken_3.0/resources/include".split(';') if "/home/dyutimoy/catkin_ws/src/kraken_3.0/resources/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "resources"
-PROJECT_SPACE_DIR = "/home/dyutimoy/catkin_ws/devel/.private/resources"
-PROJECT_VERSION = "1.0.0"
