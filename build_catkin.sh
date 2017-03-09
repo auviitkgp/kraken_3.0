0c6969eeab9f660ba61d0021a2f0fdade5e96212
Athitya Kumar <athityakumar@gmail.com>
2017-03-09 13:00:40 +0530

    Adds auto-detect repos of Catkin_ws and repo to build_catkin.sh

--- build_catkin.sh
+++ build_catkin.sh
@@ -3,13 +3,8 @@
 # 2. Will run make clean everywhere
 # 3. Will build the whole repository
 
-REPO_PATH=~/catkin_ws/src/kraken_catkin
-CATKIN_WS_PATH=~/catkin_ws
-
-cd &&
-cd $REPO_PATH &&
+cd "$(dirname "$0")" &&
 git clean -fdX &&
-cd &&
-cd $CATKIN_WS_PATH &&
+cd ../../ &&
 catkin_make clean &&
 catkin_make
